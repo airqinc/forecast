bf9147e774eb8172ebd450a89437bd9683456e54
PedroLuzon <pedroluzon2a@gmail.com>
2017-05-02 19:32:04 +0200

    Initialize app code

--- forecast/app/app.py
+++ forecast/app/app.py
@@ -1,6 +1,20 @@
 import pandas as pd
 import numpy as np
+import requests
+import sched
+import time
 
-diagnosed_data = pd.read_json()
+def predict(req, sch):
+    #requests.get(req)
+    #diagnosed_data = pd.read_json()
+    #diagnosed_data = pd.get_dummies(diagnosed_data, columns=["DayName","WindDirection"])
+    print("Hello")
+    scheduler.enter(delay, priority, predict, (req, sch,))
 
-diagnosed_data = pd.get_dummies(diagnosed_data, columns=["DayName","WindDirection"])
+if __name__ == '__main__':
+    delay = 5
+    priority = 1
+    requestDir = ""
+    scheduler = sched.scheduler(time.time, time.sleep)
+    predict(requestDir, scheduler)
+    scheduler.run()
